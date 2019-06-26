e30212696a2810e633de6bfa449bf5f25668ad55
Angel Tsvetkov <mrproper86@gmail.com>
2019-06-26 14:59:05 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function close() {
-    window.extAsyncInit = function () {
+
         MessengerExtensions.requestCloseBrowser(function success() {
             // webview closed
         }, function error(err) {
             // an error occurred
         });
-    };
+
 }
